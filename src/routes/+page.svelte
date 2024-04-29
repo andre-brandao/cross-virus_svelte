e96831942af8db42fb294e46f863fffcfe32e030
Breno Brandão <brenoca35@gmail.com>
2024-04-29 20:52:41 -0300

    Button acessar mapas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
+	import Button from '$lib/components/Button.svelte'
 	import type { PageData } from './$types'
+	import { Map } from 'lucide-svelte'
 
 	export let data: PageData
 	const municipio = data.municipio
 	console.log(municipio)
+	
 </script>
 
-<main class="flex justify-center items-center md:h-[90vh] bg-gray-100">
-    <div class="text-center">
-        <h1 class="text-3xl font-bold mb-4">Bem-vindo à Central Crossvirus de <span class="text-primary">{municipio?.nome} - {municipio?.UF}</span></h1>
-        <p class="text-lg text-gray-600">Mantenha-se seguro e informado sobre as últimas atualizações.</p>
-    </div>
+<main
+	class="flex flex-col justify-center items-center md:h-[90vh] bg-gray-100"
+>
+		<div class="text-center">
+			<h1 class="text-3xl font-bold mb-4">
+				Bem-vindo à Central Crossvirus de <span
+					class="text-primary"
+					>{municipio?.nome} - {municipio?.UF}</span
+				>
+			</h1>
+			<p class="text-lg text-gray-600">
+				Mantenha-se seguro e informado sobre as últimas
+				atualizações.
+			</p>
+		</div>
+		<div class="mt-4">
+			<Button label={`Acessar mapas de ${municipio?.nome}`} href={'/maps'} Icon={Map} />
+		</div>
 </main>
