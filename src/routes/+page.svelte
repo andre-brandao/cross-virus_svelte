b9e2e17a28ad5cfe5fc5083a534c8af9eab30175
Breno Brandão <brenoca35@gmail.com>
2024-04-29 20:41:22 -0300

    estilo pagina inicial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,9 @@
 	console.log(municipio)
 </script>
 
-<main class="flex justify-center items-center h-screen">
-	<h1>Bem vindo a central crossvirus de {municipio?.nome} - {municipio?.UF}</h1>
+<main class="flex justify-center items-center md:h-[90vh] bg-gray-100">
+    <div class="text-center">
+        <h1 class="text-3xl font-bold mb-4">Bem-vindo à Central Crossvirus de <span class="text-primary">{municipio?.nome} - {municipio?.UF}</span></h1>
+        <p class="text-lg text-gray-600">Mantenha-se seguro e informado sobre as últimas atualizações.</p>
+    </div>
 </main>
