7fb139f62fd5cd6c4ddd592b694eb4ea4be83633
Breno Brandão <brenoca35@gmail.com>
2024-05-09 20:36:22 -0300

    home page updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,8 @@
 		casos: number
 	}[] = []
 
+	let formattedDate: string
+
 	onMount(async () => {
 		const resp = await fetch('/api/casos').then((res) =>
 			res.json(),
@@ -27,79 +29,118 @@
 			}
 		})
 		console.log(casos_doenca)
+
+		formatData()
 	})
+
+	function formatData() {
+		const dateStr = municipio?.created_at
+		if (dateStr) {
+			formattedDate = new Date(dateStr).toLocaleDateString(
+				'pt-BR',
+			)
+		} else {
+			formattedDate = 'Data não disponível'
+		}
+	}
 </script>
 
 <main
-	class="flex flex-col justify-center items-center md:h-[90vh] bg-gray-100"
+	class="flex flex-col items-center md:h-[90vh] bg-gray-100"
 >
-	<h1 class="text-3xl font-bold mb-4 text-center">
-		Bem-vindo à Central Crossvirus de <span
-			class="text-primary"
-			>{municipio?.nome} - {municipio?.UF}</span
-		>
-	</h1>
-	<div class="mb-5">
-		<p class="text-2xl font-bold text-black mb-2">
-			Casos notificados de doenças:
-		</p>
-		<div
-			class="flex flex-col border-4 p-4 rounded-lg justify-center items-center"
-		>
-			{#each casos_doenca ?? [] as q_casos}
-				<div class="flex justify-between w-60">
-					<p class="text-xl text-gray-600">
-						{q_casos.doenca}:
-					</p>
-					<p class="text-xl text-secondary">
-						{q_casos.casos}
-					</p>
-				</div>
-			{/each}
-		</div>
-	</div>
-	<h1 class="font-bold text-lg text-center">
-		Mapas de {municipio?.nome}:
-	</h1>
-	<div class=" grid grid-cols-3 gap-2 mb-7 mt-3">
-		{#if maps}
-			{#each maps as mapa}
-				<a
-					href="maps/{mapa.id}"
-					class="border-2 rounded p-4"
+	<section
+		class="mt-11 text-4xl max-md:mt-10 max-md:max-w-full"
+	>
+		<h1 class="font-medium text-black text-center px-4">
+			Bem-vindo à Central Crossvirus de <span
+				class="text-primary font-bold"
+				>{municipio?.nome} - {municipio?.UF}</span
+			>
+		</h1>
+	</section>
+	<section
+		class="px-5 mt-14 w-full max-w-[1734px] max-md:mt-10 max-md:max-w-full"
+	>
+		<div class="flex flex-col md:flex-row gap-8">
+			<div class="flex flex-col flex-1">
+				<h2 class="text-2xl text-black">
+					Informações da cidade:
+				</h2>
+				<article
+					class="p-6 mt-5 bg-white rounded-lg shadow-sm text-xl"
 				>
-					<p>{mapa.title}</p>
-				</a>
-			{/each}
-		{:else}
-			<h1>Nenhum mapa registrado!</h1>
-		{/if}
-	</div>
-	<div class="mb-4">
-		<Button
-			label={`Acessar todos mapas de ${municipio?.nome}`}
-			href={'/maps'}
-			Icon={Map}
-		/>
-	</div>
+					<p class="pb-3">
+						<strong>Data de criação:</strong>
+						{formattedDate}
+					</p>
+					<p class="pb-3">
+						<strong>Código do município:</strong>
+						{municipio?.CodMun}
+					</p>
+					<p class="pb-3">
+						<strong>População estimada:</strong>
+						{municipio?.pop_est}
+					</p>
+					<p class="pb-3">
+						<strong>Faixa da população:</strong>
+						{municipio?.faixa_pop}
+					</p>
+					<p class="pb-3">
+						<strong>Região:</strong>
+						{municipio?.regiao}
+					</p>
+					<p><strong>UF:</strong> {municipio?.UF}</p>
+				</article>
+			</div>
 
-	<p class="text-xl font-bold text-gray-600 mt-3 mb-2">
-		Informacões da cidade:
-	</p>
-	<p class="text-lg text-gray-600">
-		Data de criação: {municipio?.created_at}
-	</p>
-	<p class="text-lg text-gray-600">
-		Código do município: {municipio?.CodMun}
-	</p>
-	<p class="text-lg text-gray-600">
-		População estimada: {municipio?.pop_est}
-	</p>
-	<p class="text-lg text-gray-600">
-		Faixa da população: {municipio?.faixa_pop}
-	</p>
-	<p class="text-lg text-gray-600">
-		Região: {municipio?.regiao}
-	</p>
-	<p class="text-lg text-gray-600">UF: {municipio?.UF}</p>
+			<div class="flex flex-col flex-1">
+				<h3 class="text-2xl text-black">
+					Mapas de <strong class="font-medium"
+						>{municipio?.nome}:</strong
+					>
+				</h3>
+				<div class="flex flex-col gap-5 mt-5">
+					{#if maps}
+						{#each maps as mapa}
+							<a
+								href="maps/{mapa.id}"
+								class="grow justify-center text-lg px-11 py-4 bg-white rounded-lg shadow-sm w-full max-md:px-5 hover:shadow-xl transition ease-in-out"
+							>
+								<p class="text-center">{mapa.title}</p>
+							</a>
+						{/each}
+					{:else}
+						<h1>Nenhum mapa registrado!</h1>
+					{/if}
+				</div>
+				<div class="mt-6">
+					<Button
+						label={`Acessar todos mapas de ${municipio?.nome}`}
+						href={'/maps'}
+						Icon={Map}
+					/>
+				</div>
+			</div>
+
+			<aside class="flex flex-col flex-1">
+				<h3 class="text-2xl text-black">
+					Casos notificados de doenças:
+				</h3>
+				<div
+					class="flex flex-col gap-5 justify-between px-8 py-9 mt-5 bg-white rounded-lg shadow-sm text-2xl"
+				>
+					{#each casos_doenca ?? [] as q_casos}
+						<div class="flex justify-between">
+							<p class=" text-black">
+								{q_casos.doenca}:
+							</p>
+							<p class=" text-secondary font-bold">
+								{q_casos.casos}
+							</p>
+						</div>
+					{/each}
+				</div>
+			</aside>
+		</div>
+	</section>
 </main>
